3c949bbc6da8e439991c686c860f3516a062161a
zhangzuozhanglina <12059838155@qq.com>
2020-08-11 17:10:00 +0800

    配置 p6spy

--- com.zzz.hathor.captcha/src/main/java/com/zzz/hathor/captcha/domain/entity/DicDataInfo.java
+++ com.zzz.hathor.captcha/src/main/java/com/zzz/hathor/captcha/domain/entity/DicDataInfo.java
@@ -1,21 +1,32 @@
 package com.zzz.hathor.captcha.domain.entity;
 
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
-
-import java.io.Serializable;
 
 /**
  *字典表
  */
 @Data
+@TableName(value = "dic_data_info",keepGlobalPrefix=false)
 public class DicDataInfo extends BaseInfo {
 
+    @TableId(type = IdType.AUTO)
     private String id ;
+    @TableField
     private String key ;
+    @TableField
     private String value ;
+    @TableField
+    private String clazzName;
+    @TableField
     private String desc ;
+    @TableField
     private Data createTime ;
+    @TableField
     private Boolean deprecated;
+    @TableLogic(value = "0" ,delval = "1")
+    private Boolean isDelete ;
+
 
 
 }
